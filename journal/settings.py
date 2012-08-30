fc743a879106b3af57e6ce7581c101d59856cdad
Andrew Stahlman <andrew.stahlman@gmail.com>
2012-08-30 18:24:35 -0500

    More migration

--- journal/settings.py
+++ journal/settings.py
@@ -47,30 +47,6 @@ USE_L10N = True
 USE_TZ = True
 
 # Not using this stuff... (for now)
-"""
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-"""
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
