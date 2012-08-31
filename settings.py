b343f6a16811fa4f03b923dffdf362f207dfd961
Andrew Stahlman <andrew.stahlman@gmail.com>
2012-08-30 19:07:45 -0500

    Still trying to fix imports.

--- settings.py
+++ settings.py
@@ -96,7 +96,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'journal.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -112,7 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'journal_app',
+	'journal.journal_app',
 	'gunicorn',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
