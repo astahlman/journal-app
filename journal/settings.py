516bf51870159df6b14e7e01cc1b02ebb7b86948
Andrew Stahlman <andrew.stahlman@gmail.com>
2012-08-31 00:38:13 -0500

    Fixed import error in settings.py

--- journal/settings.py
+++ journal/settings.py
@@ -49,7 +49,7 @@ USE_TZ = True
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_ROOT = join(PROJECT_PATH, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
 
 STATIC_URL = ''
 
@@ -58,7 +58,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	join(PROJECT_PATH, 'static'),
+	os.path.join(PROJECT_PATH, 'static'),
 )
 
 
@@ -103,7 +103,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	join(PROJECT_PATH, 'templates'),
+	os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
