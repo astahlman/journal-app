96d821b10d2f5cd36b112aa3f1a9a56bf389b107
Andrew Stahlman <andrew.stahlman@gmail.com>
2012-09-14 17:27:48 -0500

    Fixed formatting in db_views

--- journal/journal_app/db_views.py
+++ journal/journal_app/db_views.py
@@ -4,6 +4,6 @@ from django.utils import simplejson
 def drop_table(request):
 
 	cursor = connection.cursor()
-	cursor.execute(“DROP TABLE journal_app_node;”) 
-	success = simplejson.dumps({‘success’:’success’,})
-	return HttpResponse(success, mimetype=’application/json’)
+	cursor.execute("DROP TABLE journal_app_node;")
+	success = simplejson.dumps({'success':'success',})
+	return HttpResponse(success, mimetype='application/json')
